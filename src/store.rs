19d179371f6cb74e3330481534c538294e73e994
Anuj Srivastava <asrivas1@asrivas1-mn1.linkedin.biz>
2025-04-16 10:57:55 +0530

    Add log pointer storage and retrieval
    Change map to store index of key -> log offset instead of the previous
    key -> val mapping.
    Change get to retrieve value from the log file.

--- src/store.rs
+++ src/store.rs
@@ -1,10 +1,16 @@
 use crate::{Error, Result};
-use std::collections::{HashMap};
+use std::collections::HashMap;
 use std::path::Path;
 use std::fs;
 use std::io::{BufWriter, Write, BufReader, Read, Seek, SeekFrom};
 use serde::{Deserialize, Serialize};
 use serde_json::Deserializer;
+
+#[derive(Clone)]
+struct CmdOffset {
+    start: u64,
+    end: u64,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 enum Command {
@@ -28,7 +34,9 @@ impl Command {
 }
 
 pub struct KvStore {
-    index: HashMap<String, String>,
+    index: HashMap<String, CmdOffset>,
+    pos: u64,
+    reader: BufReader<fs::File>,
     writer: BufWriter<fs::File>,
 }
 
@@ -36,27 +44,32 @@ impl KvStore {
     pub fn open(dir: &Path) -> Result<KvStore> {
         let _ = fs::create_dir_all(dir);
         let filename = dir.join("wal.log");
-        let index = load_index(&filename)?;
-
         let writer = BufWriter::new(
             fs::OpenOptions::new()
-                .create(true)
-                .write(true)
-                .append(true)
-                .open(filename)?,
+            .create(true)
+            .write(true)
+            .append(true)
+            .open(&filename)?,
         );
+        let f = fs::File::open(&filename)?;
+        let mut reader = BufReader::new(f);
+        let (index, pos) = load_index(&mut reader)?;
 
-        Ok(KvStore{writer, index})
+        Ok(KvStore{pos, reader, writer, index})
     }
 
     pub fn set(&mut self, key: String, val: String) -> Result<()> {
         let cmd = Command::init_set(key.clone(), val.clone());
         let serialized = serde_json::to_string(&cmd).unwrap();
+        let b = serialized.as_bytes();
+        let end_pos = self.pos + b.len() as u64;
 
-        self.index.insert(key, val);
+        self.index.insert(key, CmdOffset{start: self.pos, end: end_pos});
 
-        self.writer.write(serialized.as_bytes())?;
+        self.writer.write(b)?;
         self.writer.flush()?;
+
+        self.pos = end_pos;
 
         Ok(())
     }
@@ -68,41 +81,52 @@ impl KvStore {
 
         let cmd = Command::init_rm(key.clone());
         let serialized = serde_json::to_string(&cmd).unwrap();
+        let b = serialized.as_bytes();
 
         self.index.remove(&key);
 
-        self.writer.write(serialized.as_bytes())?;
+        self.writer.write(b)?;
         self.writer.flush()?;
+
+        self.pos += b.len() as u64;
 
         Ok(key)
     }
 
-    pub fn get(&self, key: String) -> Result<Option<String>> {
+    pub fn get(&mut self, key: String) -> Result<Option<String>> {
         if !self.index.contains_key(&key) {
             return Ok(None);
         }
         
-        Ok(Some(self.index[&key].clone()))
+        let offset = self.index[&key].clone();
+        println!("seeking for key: {}, offset start: {}, end: {}", key, offset.start, offset.end);
+        let _ = self.reader.seek(SeekFrom::Start(offset.start));
+        let reader = (&mut self.reader).take(offset.end - offset.start);
+
+        if let Command::Set{val, ..} = serde_json::from_reader(reader)? {
+            Ok(Some(val))
+        } else {
+            Ok(None)
+        }
     }
 }
 
-fn load_index(file: &Path) -> Result<HashMap<String, String>> {
+fn load_index(reader: &mut BufReader<fs::File>) -> Result<(HashMap<String, CmdOffset>, u64)> {
     let mut index = HashMap::new();
-    let f = match fs::File::open(file) {
-        Ok(f) => f,
-        Err(_) => return Ok(index),
-    };
-    let mut reader = BufReader::new(f);
 
-    let _  = reader.seek(SeekFrom::Start(0))?;
+    let mut cmd_start = reader.seek(SeekFrom::Start(0))?;
     let mut stream = Deserializer::from_reader(reader).into_iter::<Command>();
+    let mut pos = 0;
 
     while let Some(cmd) = stream.next() {
+        let cmd_end = stream.byte_offset() as u64;
         match cmd? {
-            Command::Set {key, val} => index.insert(key, val),
+            Command::Set {key, ..} => index.insert(key, CmdOffset{start: cmd_start, end: cmd_end}),
             Command::Rm {key} => index.remove(&key),
         };
+        cmd_start = cmd_end;
+        pos = cmd_end;
     }
 
-    Ok(index)
+    Ok((index, pos))
 }
